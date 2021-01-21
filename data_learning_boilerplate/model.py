65bcb06ec0c044d57c0e5f4e29f2651abe44eb51
Jacky Liang <jackyliang@cmu.edu>
2021-01-21 11:09:10 -0500

    working  model training

--- data_learning_boilerplate/model.py
+++ data_learning_boilerplate/model.py
@@ -9,7 +9,7 @@ from pytorch_lightning import LightningModule
 import wandb
 
 
-def _make_mlp(in_size, layer_sizes, act='tanh', last_act=True, dropout=0, prefix=''):
+def _make_mlp(in_size, layer_sizes, act='relu', last_act=True, dropout=0, prefix=''):
     if act =='tanh':
         act_f = nn.Tanh()
     elif act == 'relu':
@@ -43,117 +43,30 @@ class MlpModel(LightningModule):
         if ds is not None:
             self._ds = ds
 
-        self._make_model()
+        self._mlp = _make_mlp(cfg['x_dim'], cfg['hidden_layers'] + [cfg['y_dim']], last_act=False)
 
-    def _make_model(self):
-        self._encoder = _make_mlp(self._cfg['n_data'] + self._cfg['n_cond'],
-                                    self._cfg['encoder'], prefix='encoder')
-
-        self._hidden_dim = self._cfg['encoder'][-1]
-        self._fc_mu = _make_mlp(self._hidden_dim, [self._cfg['n_latent']], prefix='fc_mu', last_act=False)
-        self._fc_var = _make_mlp(self._hidden_dim, [self._cfg['n_latent']], prefix='fc_var', last_act=False)
-
-        self._decoder = _make_mlp(self._cfg['n_latent'] + self._cfg['n_cond'], 
-                        list(self._cfg['decoder']) + [self._cfg['n_data']],
-                        prefix='encoder', last_act=False)
-
-    @property
-    def encoder(self):
-        return self._encoder
-
-    @property
-    def decoder(self):
-        return self._decoder
-
-    def encode(self, data_cond):
-        result = self.encoder(data_cond)
-        
-        mu = self._fc_mu(result)
-        log_var = self._fc_var(result)
-
-        return mu, log_var
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return eps * std + mu, eps
-
-    def decode(self, z):
-        result = self._decoder(z)
-        return result
-
-    def forward(self, x, cond):
-        data_cond = torch.cat([x, cond], dim=1)
-        mu, log_var = self.encode(data_cond)
-
-        z, eps = self.reparameterize(mu, log_var)
-        z_cond = torch.cat([z, cond], dim=1)
-
-        x_hat = self.decode(z_cond)
-
-        nll = -torch.sum(self._normal_distr.log_prob(eps), dim=1)
+    def forward(self, x):
+        y_hat = self._mlp(x)
 
         return {
-            'x_hat': x_hat,
-            'mu': mu,
-            'log_var': log_var,
-            'nll': nll
+            'y_hat': y_hat
         }
 
-    def sample(self, cond, num_samples, truncate=1):
-        rvs = truncnorm.rvs(-truncate, truncate, size=(num_samples, self._cfg['n_latent']))
-        nll = from_numpy(-np.sum(truncnorm.logpdf(rvs, -3, 3), axis=1)).type_as(cond)
-        
-        z = from_numpy(rvs).type_as(cond)
-        z_cond = torch.cat([z, cond.repeat(num_samples, 1)], dim=1)
-        x_hats = self.decode(z_cond)
+    def _criterion(self, y_hat, y):
+        loss = F.mse_loss(y_hat, y)
         return {
-            'x_hats': x_hats,
-            'nlls': nll
+            'loss': loss
         }
-
-    def generate(self, x, cond):
-        return self.forward(x, cond)['x_hat']
 
     def training_step(self, batch, batch_idx):
-        outs = self.forward(batch['x'], batch['cond'])
-        criterion_outs = self._criterion(batch['x'], outs['x_hat'], outs['mu'], outs['log_var'])
+        outs = self.forward(batch['x'])
+        criterion_outs = self._criterion(outs['y_hat'], batch['y'])
 
         for k, v in criterion_outs.items():
             if v.dim() == 0:
                 self.log(f'tr/{k}', v, on_step=True)
 
         return {'loss': criterion_outs['loss']}
-
-    def _criterion_pred(self, x, x_hat):
-        recons_loss = F.mse_loss(x_hat, x)
-        
-        n_state = self._cfg['n_state']
-        state_loss = F.mse_loss(x_hat[:, :n_state], x[:, :n_state])
-        exec_time_loss = F.mse_loss(x_hat[:, n_state], x[:, n_state])
-        exec_cost_loss = F.mse_loss(x_hat[:, n_state + 1], x[:, n_state + 1])
-        plan_time_loss = F.mse_loss(x_hat[:, n_state + 2], x[:, n_state + 2])
-
-        return {
-            'recons_loss': recons_loss,
-            'state_loss': state_loss,
-            'exec_time_loss': exec_time_loss,
-            'exec_cost_loss': exec_cost_loss,
-            'plan_time_loss': plan_time_loss
-        }
-
-    def _criterion(self, x, x_hat, mu, log_var):
-        pred_losses = self._criterion_pred(x, x_hat)
-        kld_loss = -0.5 * torch.sum(1 + log_var - mu ** 2 - log_var.exp()) / x.shape[0]
-
-        loss = pred_losses['recons_loss'] + kld_loss * self._cfg['klscale']
-
-        criterion_outs = {
-            'loss': loss, 
-            'kld_loss': kld_loss
-        }
-        criterion_outs.update(pred_losses)
-        return criterion_outs
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self._cfg['lr'])
@@ -165,11 +78,8 @@ class MlpModel(LightningModule):
         return DataLoader(self._ds, batch_size=self._cfg['batch_size'], sampler=SubsetRandomSampler(self._ds.test_idxs))
 
     def validation_step(self, batch, batch_idx):
-        outs = self.forward(batch['x'], batch['cond'])
-        criterion_outs = self._criterion(batch['x'], outs['x_hat'], outs['mu'], outs['log_var'])
-        criterion_outs.update(batch)
-        criterion_outs.update(outs)
-
+        outs = self.forward(batch['x'])
+        criterion_outs = self._criterion(outs['y_hat'], batch['y'])
         return criterion_outs
 
     def validation_epoch_end(self, all_outputs):
