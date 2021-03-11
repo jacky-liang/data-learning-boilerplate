72d328913ff0b4b9e9253cf5db0f11cc3104962d
Jacky Liang <jackyliang@cmu.edu>
2021-03-11 11:02:59 -0500

    updated for latest ptl version added early stopping added saving latest checkpoint

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import hydra
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 
 from data_learning_boilerplate.model import MlpModel
@@ -19,24 +19,39 @@ def main(cfg):
     hydra_dir = Path(os.getcwd())
     checkpoint_path = hydra_dir / 'checkpoints'
 
-    callbacks = []
+    # Make logger 
     wandb_logger = WandbLogger(name=cfg['tag'], config=cfg['train'], **cfg['wandb']['logger'])
     _ = wandb_logger.experiment # to explicitly trigger wandb init
-    callbacks.append(WandbUploadCallback(
+
+    # Make callbacks
+    wandb_upload_callback = WandbUploadCallback(
         checkpoint_path, hydra_dir, cfg['wandb']['saver']['upload']
-    ))
+    )
+    early_stop_callback = EarlyStopping(
+        monitor=cfg['wandb']['saver']['monitor'],
+        min_delta=0.0,
+        patience=3,
+        mode='min'
+    )
     checkpoint_callback = ModelCheckpoint(
         monitor=cfg['wandb']['saver']['monitor'],
-        filepath=checkpoint_path / '{epoch:04d}-{val_loss:.4f}',
-        save_top_k=3
+        dirpath=str(checkpoint_path),
+        filename='{epoch:04d}-{val_loss:.4f}',
+        save_top_k=3,
+        save_last=True
     )
+
+    # Make trainer and start straining
     trainer = Trainer(
         gpus=[0],
         fast_dev_run=cfg['debug'],
-        logger=wandb_logger,
-        checkpoint_callback=checkpoint_callback,
         max_epochs=cfg['train']['max_epochs'],
-        callbacks=callbacks,
+        logger=wandb_logger,
+        callbacks=[
+            wandb_upload_callback,
+            early_stop_callback,
+            checkpoint_callback
+        ],
     )
 
     ds = MockDataset(Path(cfg['root_dir']), cfg['data'])
